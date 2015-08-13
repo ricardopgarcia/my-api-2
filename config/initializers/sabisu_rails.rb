39b0df878ea80d1ea9b6562cfb9a7104eb5f8756
Ricardo Garcia <ricardopgarcia@gmail.com>
2015-08-13 00:53:32 +0000

    Initial Commit

---
+++ config/initializers/sabisu_rails.rb
@@ -0,0 +1,44 @@
+# Use this module to configure the sabisu available options
+
+SabisuRails.setup do |config|
+
+  # Base uri for posting the 
+  # config.base_api_uri = nil 
+  #config.base_api_uri = 'api.ricardopgarcia.noip.me:3000'
+  config.base_api_uri = 'my-api-2.dev'
+
+  # Ignored attributes for building the forms
+  # config.ignored_attributes = %w{ created_at updated_at id }
+  
+  # HTTP methods
+  # config.http_methods = %w{ GET POST PUT DELETE PATCH }
+  
+  # Headers to include on each request
+  #
+  # You can configure the api headers fairly easy by just adding the correct headers
+  # config.api_headers = { "Accept" => "application/json,application/vnd.application.v1" }
+  config.api_headers = { "Accept" => "application/vnd.my-api-1.v1" }
+  #
+  # config.api_headers = {}
+
+  # Layout configuration
+  # config.layout = "sabisu"
+
+  # Resources on the api
+  # config.resources = [:products, :users...]
+  config.resources = [:users]
+  # Default resource
+  config.default_resource = :users
+
+  # Application name
+  # mattr_accessor :app_name
+  # @@app_name = Rails.application.class.parent_name
+
+  # Authentication
+  # mattr_accessor :authentication_username
+  # @@authentication_username = "admin"
+
+  # mattr_accessor :authentication_password
+  # @@authentication_password = "sekret"
+
+end
